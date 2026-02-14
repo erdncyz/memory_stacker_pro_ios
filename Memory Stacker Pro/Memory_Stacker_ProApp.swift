940f5c1347efea575bcb6bdea93873642ea7f33e
Erdinç Yılmaz <erdinc.yilmaz@digiturk.com.tr>
2026-02-14 21:12:47 +0300

    Initial Commit

---
+++ Memory Stacker Pro/Memory_Stacker_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_Stacker_ProApp.swift
+//  Memory Stacker Pro
+//
+//  Created by Erdinç Yılmaz on 14.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Memory_Stacker_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
